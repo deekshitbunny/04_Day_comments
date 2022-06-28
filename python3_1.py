a9d9664ef5eef6939f32e95df1d236be8f76600c
Deekshitth Vegi <65802453+deekshitbunny@users.noreply.github.com>
2022-06-28 16:57:34 +0530

    Add files via upload

---
+++ python3_1.py
@@ -0,0 +1,11 @@
+#single line comment
+# print('I love watching anime')
+# print('Luffy is my favourite main character')
+
+'''multiple line comment'''
+'''print('I love watching anime')
+print('Luffy is my favourite main character')'''
+
+"""Another multiple line comment"""
+"""print('I love watching anime')
+print('Luffy is my favourite main character')"""
